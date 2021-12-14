78058b3e2c38dcee3a4d2901ae94e41023d5bc3b
Kaung Sithu <kgcthu.kev@gmail.com>
2021-12-14 14:31:35 +0630

    Navbar and Footer component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes } from 'react-router-dom'
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+
+        <main className='container mx-auto px-3 pb-12'>Content</main>
+        <Footer />
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: { extend: {} },
+  plugins: [require('daisyui')],
+}
